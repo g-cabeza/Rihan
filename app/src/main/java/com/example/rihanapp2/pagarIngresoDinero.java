461228ff0ada0be59af34597c9760b1e852b6f6b
AstraAbigail <astrabigail@gmail.com>
2021-10-20 20:58:42 -0300

    Activitys  de pagar Iconos de configuracion inicial imagenes Menu principal el color por el ratio

---
+++ app/src/main/java/com/example/rihanapp2/pagarIngresoDinero.java
@@ -0,0 +1,14 @@
+package com.example.rihanapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pagarIngresoDinero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagar_ingreso_dinero);
+    }
+}
---
+++ app/src/main/java/com/example/rihanapp2/pagar_2.java
@@ -0,0 +1,14 @@
+package com.example.rihanapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pagar_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagar2);
+    }
+}
